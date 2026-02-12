44b49777e71657e449b2f819fa0cc29f122ab86c
amdymstfa <amdymoustapha011@gmail.com>
2026-02-12 21:41:38 +0100

    feat(app): add navbar, footer and router-outlet in root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {RegisterComponent} from "./features/auth/components/register/register.component";
+import {NavbarComponent} from "./shared/components/navbar/navbar.component";
+import {FooterComponent} from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegisterComponent],
+  imports: [RouterOutlet, RegisterComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
